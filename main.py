288e22a56537c459f8f7ab45e9aa40b543209288
User <sup@eaeeae.sdada>
2018-08-26 02:28:24 +0300

    added comments

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ from aim import Aim
 
 aim = Aim()
 while True:
+    #6 - номер доп. кнопки, замените на нужную
     if windll.user32.GetKeyState(6) != 0 and windll.user32.GetKeyState(6) != 1 and aim.stop:
         print('started')
         aim = Aim()
         aim.start()
+    #6 - номер доп. кнопки, замените на нужную
     elif windll.user32.GetKeyState(6) != 0 and windll.user32.GetKeyState(6) != 1:
         print('stoped')
         aim._stop()
